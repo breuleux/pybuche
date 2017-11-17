eb00165d2590be71efeb75eaf48d4075d1e6f7d6
Olivier Breuleux <breuleux@gmail.com>
2017-11-17 14:44:49 -0500

    0.1.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='buche',
-    version='0.1.3',
+    version='0.1.4',
 
     description='Pretty-print Python objects to Buche logger.',
     long_description=long_description,
